664e5c1475de4cd07f5fb25854a90af30779f796
배희수 <huisu0715@khu.ac.kr>
2019-10-05 16:19:47 +0900

    프로젝트 기본 구조 생성

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!\n');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
