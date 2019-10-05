5969da3c41ffebc4de30c492ea587bd11db5fee7
배희수 <huisu0715@khu.ac.kr>
2019-10-05 17:47:44 +0900

    Goods API implemented

--- routes/goods.js
+++ routes/goods.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../lib/db');
+
+router.get('/', (req, res) => {
+
+    const query = "SELECT id, musician_id, name, price FROM Goods";
+
+    db.query(query, (err, rows, fields) => {
+
+        if(err) {
+            res.json({
+                msg: "Error! Lookup Process Failed."
+            })
+        }
+        else {
+            res.json(JSON.parse(JSON.stringify(rows)));
+        }
+    })
+})
 
 module.exports = router;
